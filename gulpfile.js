1884ab0e461673dc24a8f1904ac8e3cf66508988
Ermioni <e.krits1993@gmail.com>
2017-11-16 09:32:30 +0200

    Completed my band section,wrapper and mobile-first

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+
+// var gulp = require('gulp');
+// gulp.task('default', function(){
+//     console.log("hello ermo");
+// });
+
+// gulp.task('html', function(){
+//     console.log("New HTML was saved !!");
+// });
+
+
+
+
+
+
